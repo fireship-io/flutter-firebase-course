3b693d9ad11adeb646bfbdd6d3e0b30bca493716
Rolly <rolly@narcodic.com>
2021-07-07 15:39:30 +0300

    feat: create shared app package

---
+++ packages/shared/lib/shared.dart
@@ -0,0 +1,8 @@
+library shared;
+
+export 'package:json_annotation/json_annotation.dart';
+export 'package:equatable/equatable.dart';
+export 'package:bloc/bloc.dart';
+export 'package:meta/meta.dart';
+
+export 'src/shared.dart';
