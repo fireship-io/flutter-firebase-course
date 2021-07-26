e700300099e895b1e488995c295971a025260ffd
Rolly <rolly@narcodic.com>
2021-07-26 12:20:06 +0300

    feat: add profile view

---
+++ lib/profile/view/profile_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Profile view...'),
+    );
+  }
+}
