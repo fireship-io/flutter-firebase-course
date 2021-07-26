670f84a3f0bf90f4092faa930485b01204359f93
Rolly <rolly@narcodic.com>
2021-07-26 12:18:30 +0300

    feat: add about view

---
+++ lib/about/view/about_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AboutView extends StatelessWidget {
+  const AboutView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('About this app...'),
+    );
+  }
+}
