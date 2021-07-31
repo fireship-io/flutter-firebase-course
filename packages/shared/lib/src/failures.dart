7ba5dafcc9c76fcc44ade57b8ccbf6d64a2fbed6
Rolly <rolly@narcodic.com>
2021-08-01 02:44:19 +0300

    refactor(shared): remove public ctors on failures

--- packages/shared/lib/src/failures.dart
+++ packages/shared/lib/src/failures.dart
@@ -1,5 +1,5 @@
 class AppFailure implements Exception {
-  const AppFailure();
+  const AppFailure._();
 
   factory AppFailure.fromAuth() => const AuthFailure();
   factory AppFailure.fromLogOut() => const LogOutFailure();
@@ -9,53 +9,53 @@ class AppFailure implements Exception {
   factory AppFailure.fromSignInWithAppleNotSupported() =>
       const AppleSignInNotSupportedFailure();
 
-  static const none = NoAppFailure();
+  static const none = AppNoFailure();
 
   bool get requiresReauthentication {
     return this is AuthFailure;
   }
 }
 
-class NoAppFailure extends AppFailure {
-  const NoAppFailure();
+class AppNoFailure extends AppFailure {
+  const AppNoFailure() : super._();
 }
 
 class AuthFailure extends AppFailure {
-  const AuthFailure();
+  const AuthFailure() : super._();
 }
 
 class LogOutFailure extends AppFailure {
-  const LogOutFailure();
+  const LogOutFailure() : super._();
 }
 
 class AnonymousSignInFailure extends AppFailure {
-  const AnonymousSignInFailure();
+  const AnonymousSignInFailure() : super._();
 }
 
 class GoogleSignInFailure extends AppFailure {
-  const GoogleSignInFailure();
+  const GoogleSignInFailure() : super._();
 }
 
 class AppleSignInFailure extends AppFailure {
-  const AppleSignInFailure();
+  const AppleSignInFailure() : super._();
 }
 
 class AppleSignInNotSupportedFailure extends AppFailure {
-  const AppleSignInNotSupportedFailure();
+  const AppleSignInNotSupportedFailure() : super._();
 }
 
 class TopicsFailure implements Exception {
-  const TopicsFailure();
+  const TopicsFailure._();
 
   factory TopicsFailure.fromGetTopics() => const GetTopicsFailure();
 
-  static const none = NoTopicsFailure();
+  static const none = TopicsNoFailure();
 }
 
-class NoTopicsFailure extends AppFailure {
-  const NoTopicsFailure();
+class TopicsNoFailure extends TopicsFailure {
+  const TopicsNoFailure() : super._();
 }
 
 class GetTopicsFailure extends TopicsFailure {
-  const GetTopicsFailure();
+  const GetTopicsFailure() : super._();
 }
