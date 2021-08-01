abbeae006025b7eae9d850ec63180e7dc31e2a94
Rolly <rolly@narcodic.com>
2021-08-01 04:45:36 +0300

    feat: add colors

--- lib/shared/styles.dart
+++ lib/shared/styles.dart
@@ -4,7 +4,9 @@ const kWhite = Colors.white;
 const kLightBlack = Colors.black45;
 const kGrey = Colors.grey;
 const kPurple = Color(0xFFB39DDB);
+const kDeepPurple = Colors.deepPurple;
 const kPink = Color(0xFFF48FB1);
+const kRed = Colors.red;
 
 const kMargin = 30.0;
 const kInsets = EdgeInsets.all(kMargin);
