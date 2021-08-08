c2d036cd580c9ce6569d7ce7745c2f4094698954
Rolly <rolly@narcodic.com>
2021-08-09 01:43:01 +0300

    feat: preload topic covers

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,17 +1,20 @@
 import 'dart:async';
 import 'dart:developer';
 
-import 'package:flutter/widgets.dart';
-
+import 'package:assets_repository/assets_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:quizapp/app/app.dart';
+import 'package:quizapp/shared/constants.dart';
 import 'package:user_repository/user_repository.dart';
-
-import 'app/app.dart';
 
 void mainCommon(AppEnvironment environment) {
   runZonedGuarded(
     () async {
       final bootstrapper = AppBootstrapper(environment: environment);
       await bootstrapper.bootstrapApp();
+
+      final assetsRepository = AssetsRepository();
+      await assetsRepository.preloadAssets(kCoversPath);
 
       final userRepository = FirebaseUserRepository();
       await userRepository.getOpeningUser();
