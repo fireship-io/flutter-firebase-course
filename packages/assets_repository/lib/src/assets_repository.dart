03381d6f9b83d1b3466bdf8e325ccbbc2ad0f973
Rolly <rolly@narcodic.com>
2021-08-09 01:40:50 +0300

    feat(assets_repository): preload assets

--- packages/assets_repository/lib/src/assets_repository.dart
+++ packages/assets_repository/lib/src/assets_repository.dart
@@ -1,1 +1,20 @@
-class AssetsRepository {}
+import 'dart:convert';
+
+import 'package:assets_repository/src/utils.dart';
+import 'package:flutter/services.dart';
+
+class AssetsRepository {
+  Future<void> preloadAssets(String path) async {
+    try {
+      final pathNames = await _readAssetsPathName();
+      final assets = pathNames.where((pathName) => pathName.startsWith(path));
+      await Future.wait(assets.map(preloadAsset));
+    } catch (_) {}
+  }
+
+  Future<Iterable<String>> _readAssetsPathName() async {
+    final manifestRawJson = await rootBundle.loadString('AssetManifest.json');
+    final manifestJson = json.decode(manifestRawJson) as Map<String, dynamic>;
+    return manifestJson.keys;
+  }
+}
