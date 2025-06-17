f25d96643a075113528f2bb318d19de41ca4e489
Rolly <rolly@narcodic.com>
2025-06-18 02:11:02 +0300

    style: change models formatting

--- packages/quizzes_repository/lib/src/models/models.g.dart
+++ packages/quizzes_repository/lib/src/models/models.g.dart
@@ -7,27 +7,29 @@ part of 'models.dart';
 // **************************************************************************
 
 Option _$OptionFromJson(Map<String, dynamic> json) => Option(
-      value: json['value'] as String,
-      detail: json['detail'] as String? ?? '',
-      correct: json['correct'] as bool?,
-    );
+  value: json['value'] as String,
+  detail: json['detail'] as String? ?? '',
+  correct: json['correct'] as bool?,
+);
 
 Question _$QuestionFromJson(Map<String, dynamic> json) => Question(
-      text: json['text'] as String,
-      options: (json['options'] as List<dynamic>?)
-              ?.map((e) => Option.fromJson(e as Map<String, dynamic>))
-              .toList() ??
-          [],
-    );
+  text: json['text'] as String,
+  options:
+      (json['options'] as List<dynamic>?)
+          ?.map((e) => Option.fromJson(e as Map<String, dynamic>))
+          .toList() ??
+      [],
+);
 
 Quiz _$QuizFromJson(Map<String, dynamic> json) => Quiz(
-      id: json['id'] as String,
-      title: json['title'] as String,
-      description: json['description'] as String,
-      topic: json['topic'] as String,
-      video: json['video'] as String? ?? '',
-      questions: (json['questions'] as List<dynamic>?)
-              ?.map((e) => Question.fromJson(e as Map<String, dynamic>))
-              .toList() ??
-          [],
-    );
+  id: json['id'] as String,
+  title: json['title'] as String,
+  description: json['description'] as String,
+  topic: json['topic'] as String,
+  video: json['video'] as String? ?? '',
+  questions:
+      (json['questions'] as List<dynamic>?)
+          ?.map((e) => Question.fromJson(e as Map<String, dynamic>))
+          .toList() ??
+      [],
+);
--- packages/topics_repository/lib/src/models/models.g.dart
+++ packages/topics_repository/lib/src/models/models.g.dart
@@ -7,17 +7,18 @@ part of 'models.dart';
 // **************************************************************************
 
 Quiz _$QuizFromJson(Map<String, dynamic> json) => Quiz(
-      id: json['id'] as String,
-      title: json['title'] as String,
-      description: json['description'] as String,
-    );
+  id: json['id'] as String,
+  title: json['title'] as String,
+  description: json['description'] as String,
+);
 
 Topic _$TopicFromJson(Map<String, dynamic> json) => Topic(
-      id: json['id'] as String,
-      title: json['title'] as String,
-      description: json['description'] as String? ?? '',
-      imageName: json['img'] as String? ?? 'default-cover.png',
-      quizzes: (json['quizzes'] as List<dynamic>)
+  id: json['id'] as String,
+  title: json['title'] as String,
+  description: json['description'] as String? ?? '',
+  imageName: json['img'] as String? ?? 'default-cover.png',
+  quizzes:
+      (json['quizzes'] as List<dynamic>)
           .map((e) => Quiz.fromJson(e as Map<String, dynamic>))
           .toList(),
-    );
+);
