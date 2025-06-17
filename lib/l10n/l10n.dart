7d039776960a3c2c5ba8aaabe002ce47d122c4aa
Rolly <rolly@narcodic.com>
2025-06-18 02:09:07 +0300

    chore: fix localizations export

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -6,9 +6,9 @@
 // https://opensource.org/licenses/MIT.
 
 import 'package:flutter/widgets.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:quizapp/l10n/arb/app_localizations.dart';
 
-export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+export 'package:quizapp/l10n/arb/app_localizations.dart';
 
 extension AppLocalizationsX on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this)!;
